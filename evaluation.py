e1f3ef679a7bae461e928539c361c0d0eb3f2f28
yyao666 <148615995+yyao666@users.noreply.github.com>
2026-04-07 16:22:56 +0800

    Create evaluation.py, extend

---
+++ evaluation.py
@@ -0,0 +1,9 @@
+"""
+Evaluation script for multimodal deception detection.
+
+This script can be extended to:
+- Load trained checkpoints
+- Evaluate on test datasets
+- Compute additional metrics
+- Export predictions
+"""
